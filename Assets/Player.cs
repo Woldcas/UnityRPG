257ef13c970f3451187e3d8d303b848a3d6981be
Woldcas <siyarerbay62@gmail.com>
2025-10-26 21:03:23 +0300

    0.1
    Currently developing Player State Machine

---
+++ Assets/Player.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator anim { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    public PlayerInputSet input { get; private set; }
+
+    private StateMachine stateMachine;
+
+    public Player_IdleState idleState { get; private set; }
+    public Player_MoveState moveState { get; private set; }
+    public Player_JumpState jumpState { get; private set; }
+    public Player_FallState fallState { get; private set; }
+
+
+    [Header("Movement Detalis")]
+    public float moveSpeed;
+    public float jumpForce = 5f;
+    
+    [Range(0, 1)]
+    public float inAirMoveMultiplier = 0.7f; //Should be from 0 to 1
+    private bool facingRight = true;
+    private int facingDir = 1;
+    public Vector2 moveInput { get; private set; }
+
+    [Header("Collision Detection")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private float wallCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    public bool groundDetected { get; private set; }
+
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        stateMachine = new StateMachine();
+        input = new PlayerInputSet();
+
+        idleState = new Player_IdleState(this, stateMachine, "idle");
+        moveState = new Player_MoveState(this, stateMachine, "move");
+        jumpState = new Player_JumpState(this, stateMachine, "jumpFall");
+        fallState = new Player_FallState(this, stateMachine, "jumpFall");
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+
+        input.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        input.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
+
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Start()
+    {
+        stateMachine.Initialize(idleState);
+    }
+
+    private void Update()
+    {
+        HandleCollisionDetection();
+        stateMachine.UpdateActiveState();
+    }
+
+    public void SetVelocity(float xVelocity, float yVelocity)
+    {
+        rb.linearVelocity = new Vector2(xVelocity, yVelocity);
+        HandleFlip(xVelocity);
+    }
+
+    private void HandleFlip(float xVelocity)
+    {
+        if(xVelocity > 0 && !facingRight)
+            Flip();
+        else if(xVelocity < 0 && facingRight)
+            Flip();
+
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+        facingDir = facingDir * -1;
+    }
+
+    private void HandleCollisionDetection()
+    {
+        groundDetected = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(wallCheckDistance, 0));
+    }
+}
